a51dcb7a22308981e9495684ed4a4c3358bdd5f9
buhhunyx@tut.by <buhhunyx@tut.by>
2012-02-19 23:48:13 +0200

    changed key generation

--- key-client/src/key/model/KeyGenerator.java
+++ key-client/src/key/model/KeyGenerator.java
@@ -4,7 +4,8 @@ import java.util.Random;
 
 public class KeyGenerator {
 
-	private final static Random random = new Random();
+	private final static Random RANDOM = new Random();
+	private final static String SYMBOLS = "ABCDEFGHJKLMNPQRSTUVWXYZ1234567890";
 
 	private KeyGenerator() {
 	}
@@ -14,7 +15,7 @@ public class KeyGenerator {
 		for (int i = 0; i < (octet - 1); ++i) {
 			result += '-';
 			for(int j = 0; j < 4; ++j) {
-				result += random.nextInt(10);
+				result += SYMBOLS.charAt(RANDOM.nextInt(SYMBOLS.length()));
 			}
 		}
 		return result;
